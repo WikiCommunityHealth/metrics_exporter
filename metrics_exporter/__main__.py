c776b6691aff566fb1fd9d0744e9c261f8a4f433
nicolatoscan <nicolatoscan99@outlook.it>
2021-05-30 02:15:15 +0200

    metrics exporter

---
+++ metrics_exporter/__main__.py
@@ -0,0 +1,36 @@
+from .MetricsExporter import MetricsExporter
+
+me = MetricsExporter(
+        metricsGroups=[['felicità', 'gioia'], ['revertAdmin', 'reverMerde']],
+        tableName='it',
+        user='root',
+        password='root',
+        dbname='postgres',
+        host='localhost',
+        port=5432,
+        bufferSize=10000
+    )
+
+me.insertMetric(
+    1, 'pippo', '201005',
+    [{
+        'felicità': 2,
+        'gioia': 4
+    }, {
+        'revertAdmin': 2,
+        'reverMerde': 4
+    }]
+)
+
+me.insertMetric(
+    1, 'pippo', '201006',
+    [{
+        'felicità': 4,
+        'gioia': 4
+    }, {
+        'revertAdmin': 2,
+        'reverMerde': 4
+    }]
+)
+
+me.finalize()
